bb457d5731549eba5e0a76c9fc67b7942f0a7edf
Jonathas Rosa <jonathasrosa85@gmail.com>
2022-07-06 10:19:57 -0300

    Create aula.15

---
+++ aula.15/teste001.js
@@ -0,0 +1,6 @@
+let num = [5, 8, 4] //Váriaveis compostas
+num.push(1) //Acresenta um valor 
+//num.sort() Organiza os elementos em ordem crescente
+console.log(num) //Mostra o conteúdo
+console.log(`O vetor tem ${num.length} posições.`)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
